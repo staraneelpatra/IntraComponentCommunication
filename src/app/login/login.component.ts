917b1364308e012593b469f82fc96324fea1739a
staraneelpatra <staraneel@live.com>
2023-01-10 21:41:52 +0530

    Mixed up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output,OnInit } from '@angular/core';
+import { Component, EventEmitter, Output,OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
  selector: 'app-login',
@@ -9,15 +9,22 @@ export class LoginComponent implements OnInit {
 
  userName: string = '';
  @Output() login = new EventEmitter<string>();
+ @ViewChild('userName', {static:true}) loginUsername : ElementRef;
+ 
  constructor() { }
 
  ngOnInit() {
+    console.log(this.loginUsername.nativeElement.value+'from ngOnInit');
  }
 
- submit()
+ submit(userName)
  {
- console.log(this.userName)
- this.login.emit(this.userName);
+// console.log(userName.value);
+ //console.log(this.userName)
+//  this.login.emit(this.userName);
+//this.login.emit(userName.value);
+console.log(this.loginUsername.nativeElement.value);
+this.login.emit(this.loginUsername.nativeElement.value);
  }
 
 }
